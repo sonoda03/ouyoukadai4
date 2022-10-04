6ca953616dab7891b62d5bb6294318bcee2d9132
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-10-04 16:00:21 +0000

    応用課題５訂正後

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   
-    get '/search' => "searches#index"
+    get '/search' => 'searches#search'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
