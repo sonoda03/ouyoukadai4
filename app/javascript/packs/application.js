79b000e26c11e02936b5dee287718e8c7816a2ce
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-10-05 09:00:38 +0000

    いいね機能を非同期通信化完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,8 +13,11 @@ import "popper.js"
 import "bootstrap"
 
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+
